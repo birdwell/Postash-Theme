a50c2b60ad31eba0339b4255336dc36614314c86
Josh Birdwell <jbirdwell34@gmail.com>
2015-07-29 00:46:39 -0500

    update css

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
     less = require('gulp-less'),
-    path = require('path');
-    
+    path = require('path'),
+    concat = require('gulp-concat');
+
 // run
 gulp.task('default', function() {
-  
+
   // easier to read variable
   var themeName = 'timeline';
 
@@ -18,12 +19,14 @@ gulp.task('default', function() {
   //   .pipe(gulp.dest('./assets/css/'));
   gulp.src('./assets/less/**/*.less')
   .pipe(less({ paths: [ path.join(__dirname, 'less', 'includes') ]}))
-  .pipe(gulp.dest('./assets/css/'));
+  .pipe(gulp.dest('assets/css'))
+  .pipe(concat('timeline.css'))
+  .pipe(gulp.dest('./assets/css'));
 
   // uglify
   gulp.src('./assets/js/' + themeName + '.js')
     .pipe(uglify())
-    .pipe(rename(themeName + '.min.js'))          
+    .pipe(rename(themeName + '.min.js'))
     .pipe(gulp.dest('./assets/js/'));
-  
+
 });
