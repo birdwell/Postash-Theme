2deb40b160dc48bdfb565eee6a972a7224790769
Josh Birdwell <jbirdwell34@gmail.com>
2015-08-04 00:12:48 -0500

    update js

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -4,4 +4,19 @@ if(page == 'music'){
   document.body.className = 'music';
 }else if(page == 'bio'){
   document.body.className = 'bio';
+  c = document.getElementsByClassName('container');
+  c.innerHtml = "<div class='avatar'><img src=“images/avatar.png' alt='Avatar'></div><div class='content'><h1 class='title”>Josh<br>Birdwell</h1><p>
+                Put your short bio here.<br>
+                Put where you're from here.
+            </p>
+            <ul class='social-icons'>
+                <li><a class='twitter' href='#' title='Twitter'>Twitter</a></li>
+                <li><a class='facebook' href='#' title='Facebook'>Facebook</a></li>
+                <li><a class='instagram' href='#' title='Instagram'>Instagram</a></li>
+            </ul>
+            <p>
+                Web Developer Intern at <a href=“www.nextthought.com”>NextThought</a><br>
+            </p>
+            <p>Want to chat? <a href=“mailto:joshdbirdwell@gmail.com'>Email me</a>.</p>
+        </div>";
 }
